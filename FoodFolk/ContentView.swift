11befc40131e3f2f211c25ea7c44031672646566
Carlos Rojas <csrm2799rojas@icloud.com>
2022-05-07 11:09:02 -0500

    Hi! x2

--- FoodFolk/ContentView.swift
+++ FoodFolk/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("You are super cool!")
+        Text("You are super cool! hehe")
             .padding()
     }
 }
