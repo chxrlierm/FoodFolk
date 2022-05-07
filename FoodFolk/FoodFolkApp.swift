30253056cbf4f0f90db7ebe497bb0ee0d4c5966b
Carlos Rojas <csrm2799rojas@icloud.com>
2022-05-07 10:46:21 -0500

    Initial Commit

---
+++ FoodFolk/FoodFolkApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodFolkApp.swift
+//  FoodFolk
+//
+//  Created by admin on 5/7/22.
+//
+
+import SwiftUI
+
+@main
+struct FoodFolkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
