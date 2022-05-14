fec79e1cd491050bf9d70615e0a933650cd36dab
Iris Quintero <iquinterom2@gmail.com>
2022-05-13 21:43:46 -0500

    Added: struct Restaurant on DataModel file

--- FoodFolk/DataModel.swift
+++ FoodFolk/DataModel.swift
@@ -17,12 +17,28 @@ enum CusineTypes: String {
     
 }
 
-enum priceRange: String {
+enum Prices: String {
     
     case one = "$"
     case two = "$$"
     case three = "$$$"
     case four = "$$$$"
 }
+
+struct Restaurant: Identifiable {
+    
+    var id = UUID().uuidString
+    var name: String
+    var cuisineType: CusineTypes
+    var priceRange: Prices
+    var rating: Double
+    
+
+    func formattingRestaurant() -> String {
+        return "The restaurant: \(name), with a price range of \(priceRange), they offer \(cuisineType) food and beverages, beating a global rating of: \(rating) (accodring to Google Maps)"
+    }
+    
+}
+
 
 
